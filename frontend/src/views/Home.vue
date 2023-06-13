beb28affe7638f6fecf5954e68940144d248cb94
Jan <jplck@users.noreply.github.com>
2023-06-13 11:44:00 +0000

    test

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color:magenta;">
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color:darkblue;">
           Milligram
         </b-navbar-item>
       </template>
